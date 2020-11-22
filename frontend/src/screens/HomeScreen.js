cbe7658aed5079eb700d129009b7cfd9012db064
Nauman Afsar <naumanafsar45@gmail.com>
2020-11-22 15:43:03 +0500

    Pages

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
-import products from '../products'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
+  const productList = useSelector((state) => state.productList)
+
+  const { loading, products, error } = productList
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
   return (
     <>
       <h1> Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'> {error} </Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} lx={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} lx={3}>
