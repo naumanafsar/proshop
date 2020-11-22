3eb412e2de34cc61ab6d566bbb7556fc76c24164
Nauman Afsar <naumanafsar45@gmail.com>
2020-11-22 15:37:52 +0500

    Cleanup

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+
+const app = express()
+
+app.use(express.json())
+
+dotenv.config()
+
+connectDB()
+
+app.get('/', (req, res) => {
+  res.send('API is Running!')
+})
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+)
