3feaebea56054b7d336727277ea8c838e14092a0
Nauman Afsar <naumanafsar45@gmail.com>
2020-11-26 08:13:12 +0500

    Place Order Screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -8,6 +8,24 @@ import CheckoutSteps from '../components/CheckoutSteps'
 const PlaceOrderScreen = () => {
   const cart = useSelector((state) => state.cart)
 
+  const addDecimals = (num) => {
+    return Math.round((num * 100) / 100).toFixed(2)
+  }
+
+  //  Calculate prices
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  )
+
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100)
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)))
+
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2)
+
   const placeOrderHandler = () => {
     console.log(' ')
   }
